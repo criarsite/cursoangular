56082279fa6d23c482734a8eb1c09763485d44db
criarsite <oktuga@gmail.com>
2023-11-14 14:46:43 +0000

    Atualizando

---
+++ js/oop/aula.js
@@ -0,0 +1,20 @@
+// objetos -> 
+// classes -> sao modelos que representam um objeto com propriedades e metodos
+
+let pessoa = { // em javascript um objeto é uma colecao de chave e valor
+    nome: "shin",
+    idade: 25,
+ 
+}
+
+pessoa.altura = 1.75
+
+delete pessoa.idade
+
+console.log(pessoa.nome)
+
+console.log(pessoa.idade)
+console.log(pessoa)
+
+// uma funcao dentro de um objeto chamase metodo
+
