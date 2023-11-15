4ddd119d2d85f8877560cad0bd2ef4fba88702b2
criarsite <oktuga@gmail.com>
2023-11-15 21:46:28 +0000

    Atualizando

---
+++ js/importacaoeexportacao/aula.js
@@ -0,0 +1,4 @@
+ import * as funcoes from "./funcoes.js"; // importa 
+funcoes.prints("washington gomes") // chama função
+let num = funcoes.gets() // pega valor e salva em uma variavel
+console.log(num) // imprimi a variavel 
---
+++ js/importacaoeexportacao/funcoes.js
@@ -0,0 +1,9 @@
+
+
+export function gets(){
+return 10
+}
+
+export function prints(texto){
+console.log(texto)
+}
