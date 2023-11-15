b7ac41444bb3b91d2680df6eeef8164c40db2379
criarsite <oktuga@gmail.com>
2023-11-15 20:42:52 +0000

    Atualizando

--- js/funcoes/exercicios.js
+++ js/funcoes/exercicios.js
@@ -9,4 +9,12 @@ for (let i=0; i<=10; i++ ){
 }
 
 
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+for (let i= 0; i<numeros.length; i++){
+    let numero = numeros[i]
+    if (numero % 2 === 0){
+        console.log(numero)
+    }
+}
+
 
