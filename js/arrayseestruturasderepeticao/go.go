864e9d30934524a6c01a757d7c78266e7f9abcd1
criarsite <oktuga@gmail.com>
2023-11-15 21:16:42 +0000

    Atualizando

--- js/arrayseestruturasderepeticao/go.go
+++ js/arrayseestruturasderepeticao/go.go
@@ -4,14 +4,28 @@ import "fmt"
 
 func main() {
 
-	var num = []int{}
-	for i := 1; i <= 10; i++ {
-		num = append(num, i)
-		println("")
-		for i := 1; i <= 10; i++ {
-			println("")
-			fmt.Printf("%v x %v = %v ", len(num), i, (len(num) * i))
+	// var num = []int{}
+	// for i := 1; i <= 10; i++ {
+	// 	num = append(num, i)
+	// 	println("")
+	// 	for i := 1; i <= 10; i++ {
+	// 		println("")
+	// 		fmt.Printf("%v x %v = %v ", len(num), i, (len(num) * i))
 
+	// 	}
+	// }
+
+	var num = []int{}
+
+	for i := 1; i <= len(num)+1; i++ {
+		num = append(num, i)
+
+		if len(num)%2 == 0 {
+			fmt.Println(len(num))
+			if len(num) == 10000 {
+				break
+			}
 		}
+
 	}
 }
--- js/funcoes/exercicios.js
+++ js/funcoes/exercicios.js
@@ -1,20 +1,26 @@
-let num =[]
+// let num =[]
 
-for (let i=0; i<=10; i++ ){
-    num = i
-    for(let i = 1; i<=10; i++){
+// for (let i=0; i<=10; i++ ){
+//     num = i
+//     for(let i = 1; i<=10; i++){
         
-        console.log(num, "*", i, "=", num*i) 
-     }
-}
+//         console.log(num, "*", i, "=", num*i) 
+//      }
+// }
 
 
-let numeros = [1,2,3,4,5,6,7,8,9,10]
+let numeros = []
+for(let i= 0; i<100; i++){
+numeros.push(i) 
+
+
 for (let i= 0; i<numeros.length; i++){
     let numero = numeros[i]
     if (numero % 2 === 0){
         console.log(numero)
     }
 }
+}
+
 
 
